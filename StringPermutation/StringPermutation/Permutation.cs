1abe92c9fd56c186f7e8694018faf42763e5b435
M Usama Alvi <m.usamaalvi@gmail.com>
2022-02-28 01:01:34 +0500

    creating approprirate classes.

---
+++ StringPermutation/StringPermutation/Permutation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPermutation
+{
+    public class Permutation
+    {
+        public bool CheckPermutation(string word)
+        {
+            return false;
+        }
+    }
+}
